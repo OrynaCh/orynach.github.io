cb3552cd4ac50e5baa742339a071fe63505c8645
Github Action Runner <automatic@deployment.com>
2023-05-05 09:08:48 +0000

    automatic deployment

---
+++ _nuxt/static/1683277726/ua/case/2022-09-stepan/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683277726",layout:"default",error:a,serverRendered:true,routePath:"\u002Fua\u002Fcase\u002F2022-09-stepan",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
