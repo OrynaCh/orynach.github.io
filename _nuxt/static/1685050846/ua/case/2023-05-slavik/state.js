5ee914bda315349767160539476b2dfb699770f2
Github Action Runner <automatic@deployment.com>
2023-05-25 21:40:48 +0000

    automatic deployment

---
+++ _nuxt/static/1685050846/ua/case/2023-05-slavik/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1685050846",layout:"default",error:a,serverRendered:true,routePath:"\u002Fua\u002Fcase\u002F2023-05-slavik",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
